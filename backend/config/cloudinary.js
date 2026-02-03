7743bd2d4f21aa049ac3fc1538ba8e275e0e7519
Jitendra Kumar <jitendrakumarjjk10@gmail.com>
2026-02-03 18:50:51 +0530

    Update Cloudinary con

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
-  cloud_name: 'ddc7x60nm',
-  api_key: '581564763536222',
-  api_secret: 'F0b6y3L2q2hTFxC0MN2NmHA-Tq0'
+  cloud_name: 'ddc0nm',
+  api_key: '5815536222',
+  api_secret: 'F0b6y3L2q2hMN2NmHA-Tq0'
 });
 
 const storage = new CloudinaryStorage({
